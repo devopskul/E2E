47b8501ea2e65c5426842b527b6cfcc5fbe07f90
subrajitd123 <74914343+subrajitd123@users.noreply.github.com>
2021-06-12 12:06:07 +0530

    added new mail id

---
+++ Python/sample.py
@@ -0,0 +1,21 @@
+print ("Hey There")
+print ("My\nName\nIs\nSubrajit")
+print ("Who\tAre\tYou")
+"""
+print (r"\n")
+"""
+person = "subrajit dash"
+integer = 100
+person1 = "jagan"
+person2 = "dipti"
+print (id(person))
+print (person, person1, person2)
+list = ["a", "b", "c"]
+print (list)
+list = list.replace["a", "f"]
+print (list)
+"""
+rule : object name cant start with integer
+array = 1234
+rev array = -1,-2,-3
+"""
